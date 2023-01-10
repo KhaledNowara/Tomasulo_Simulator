e9cac84aecf28167d04ff965d51815b5e4106009
fr-gr <96599199+fr-gr@users.noreply.github.com>
2023-01-10 06:00:22 +0200

    register's mostafasEventListeners

--- lib/register.dart
+++ lib/register.dart
@@ -1,61 +1,77 @@
 import 'instruction.dart' as instruction;
 import 'dart:collection';
 import 'package:tuple/tuple.dart';
-class Register {
-  double value = 0;
-  String?  opID;
-  Function(Function (double))? addListener;  
 
-  String toString (){
+class Register {
+  List<Function()> MostafsListeners = <Function()>[];
+  double value = 0;
+  String? opID;
+  Function(Function(double))? addListener;
+
+  String toString() {
     return ('$value');
   }
 
-  Tuple2<double,String?> getRegister (Function (double) listner){
-    if (opID!=null){
+  Tuple2<double, String?> getRegister(Function(double) listner) {
+    if (opID != null) {
       addListener!(listner);
     }
     return Tuple2(value, opID);
   }
 
-  void waitOn (String id, Function(Function (double)) addListener){
+  void waitOn(String id, Function(Function(double)) addListener) {
     opID = id;
     addListener(listen);
     this.addListener = addListener;
   }
 
-
-  void listen (double data){
-    value = data; 
+  void listen(double data) {
+    value = data;
     opID = null;
     addListener = null;
+    notifyMostafasListeners();
   }
 
+  void addMostafasListener(Function() f) {
+    MostafsListeners.add(f);
+  }
+
+  void notifyMostafasListeners() {
+    for (Function() f in MostafsListeners) {
+      f();
+    }
+  }
+
+  void removeMostafsListner(Function(String) f) {
+    MostafsListeners.remove(f);
+  }
 }
 
-class RegisterFile{
-  HashMap registers= HashMap<String, Register>();
+class RegisterFile {
+  HashMap registers = HashMap<String, Register>();
 
-  RegisterFile(){
-    for(int i = 0; i<10; i += i---i){
+  RegisterFile() {
+    for (int i = 0; i < 10; i += i-- - i) {
       registers.putIfAbsent('R$i', () => Register());
     }
-    for(int i = 0; i<10; i += i---i){
+    for (int i = 0; i < 10; i += i-- - i) {
       registers.putIfAbsent('F$i', () => Register());
     }
   }
 
-  Tuple2<double,String?> getRegister (String regName, Function(double) f){
-      return registers[regName].getRegister(f);
+  Tuple2<double, String?> getRegister(String regName, Function(double) f) {
+    return registers[regName].getRegister(f);
   }
 
-  void waitOn (String regName, String id, Function(Function (double)) addListener){
-    registers[regName].waitOn(id,addListener);
+  void waitOn(
+      String regName, String id, Function(Function(double)) addListener) {
+    registers[regName].waitOn(id, addListener);
   }
 }
 
-void main (){
-  RegisterFile r =RegisterFile();
-  print (  r.registers['F0']);
+void main() {
+  RegisterFile r = RegisterFile();
+  print(r.registers['F0']);
 }
 
 
