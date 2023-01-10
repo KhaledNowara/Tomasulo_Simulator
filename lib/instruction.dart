5e4316bbeaf2d5516419884db45be35f080e5451
KhaledNowara <kn_nowara@outlook.com>
2023-01-10 20:36:14 +0100

    Add instruciton Queue class

--- lib/instruction.dart
+++ lib/instruction.dart
@@ -1,7 +1,12 @@
 import 'dart:collection';
 
+
+import 'package:tomasulo_simulator/reservation_station.dart';
+
 import 'register.dart' as register;
 
+
+// do not edit 
 class Instruction {
   final InstructionType _type;
   InstructionType get type => _type;
@@ -25,6 +30,36 @@ class Instruction {
 String toString(){
   return ('type : $type , target :$target, v1 : $operand1Val, v2 : $operand2Val, o1: $operand1ID, o2: $operand1ID');
 }
+}
+
+class InstructionQueue {
+Queue <Instruction> instructionQueue = Queue <Instruction> ();
+  ReservationStation add;
+  ReservationStation mult;
+  AddressUnit addressUnit;
+  
+late Instruction currentInstruction;
+  InstructionQueue(List<Instruction> l, this.add, this.mult,this.addressUnit){
+    for (Instruction i in l){
+      instructionQueue.add(i);
+    }
+    currentInstruction = instructionQueue.first;
+  }
+bool issueInstruction(Instruction i) {
+  return add.allocate(i) ||
+      mult.allocate(i) ||
+      addressUnit.allocate(i);
+}
+
+  void onClockTick(){
+      if(instructionQueue.isNotEmpty){
+    if (issueInstruction(instructionQueue.first)){
+      instructionQueue.removeFirst();
+    }
+    currentInstruction = instructionQueue.first;
+  }
+
+  }
 }
 
 enum InstructionType { add, sub, mult, div, load, store }
